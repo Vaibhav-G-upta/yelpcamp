d8e342284de342a2f18488306c552051118b7fb8
Vaibhav Gupta <43707442+Vaibhav-G-upta@users.noreply.github.com>
2023-01-03 00:56:01 +0530

    Yelpcamp website :camera_flash:

---
+++ app.js
@@ -0,0 +1,82 @@
+var express=require("express");
+var app=express();
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var User=require("./models/user");
+var flash=require("connect-flash");
+var passport=require("passport");
+var LocalStrategy=require("passport-local")
+var passportLocalmongoose=require("passport-local-mongoose");
+var methodOverride=require("method-override");
+var campground=require("./models/campgrounds");
+var Comment=require("./models/Comments");
+var seedDB=require("./seed");
+
+// seedDB();
+
+
+app.use(flash());
+//passport requirements
+app.use(require("express-session")({
+    secret:"HO HO HO santa returns",
+    resave:false,
+    saveUninitialized:false,
+   
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.CurrentUser= req.user;
+    res.locals.error=req.flash("error"); 
+    res.locals.success=req.flash("success"); 
+    next();
+})
+
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
+app.set("view engine","ejs");
+
+mongoose.set('useFindAndModify', false);
+
+var commentroutes   =require("./routes/comments"),
+    authroutes      =require("./routes/auth"),
+    campgroundroutes=require("./routes/campgrounds");
+
+
+app.use(commentroutes);
+app.use(authroutes);
+app.use("/campgrounds",campgroundroutes);    
+app.use("/campgrounds/:id/comment",commentroutes);
+
+mongoose.connect("mongodb+srv://Admin:Admin@cluster0.jylly.mongodb.net/yelpcamp?retryWrites=true&w=majority", {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("error"+" ");
+console.log(err);
+});
+
+
+
+
+app.get("/",function(req,res){
+    res.render("landing");
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,function(){
+    console.log(`yelpcamp is started on port ${PORT}`);
+});
+
